685dd0cfed75ba3ea993764db4d28ca9b8b7dcbd
Abdellatif <155848361+latoufC@users.noreply.github.com>
2024-10-05 13:20:10 +0100

    Update source.c

--- ADC/source.c
+++ ADC/source.c
@@ -1,19 +1,19 @@
-#include <p18f452.h>
-#pragma config WDT = OFF
+#include <p18f452.h>					// PIC18F452 HEADER FILE
+#pragma config WDT = OFF				// WATCH DOG OFF
 
-unsigned char flag = 0x00;
+unsigned char flag = 0x00;				// FLAG 
 
-void acquisitionTime(void);
+void acquisitionTime(void);				// ACQUISITION TIME NEEDED FOR CAPACITOR CHARGING
 
 #pragma interrupt myFunction
 void myFunction(void)
 {
-	if(PIR1bits.ADIF)
+	if(PIR1bits.ADIF)				// CHECK IF ADC TERMINATES CONVERSION ?
 	{
-		PIR1bits.ADIF = 0;
-		LATD = ADRESL;
-		LATB = ADRESH;
-		flag = 1;
+		PIR1bits.ADIF = 0;			// CLEAR INTERRUPT FLAG
+		LATD = ADRESL;				// LOWEST BYTE IN PORTD
+		LATB = ADRESH;				// HIGHEST BYTE IN PORTC
+		flag = 1;				// FLAG 
 	}
 }
 
@@ -29,19 +29,19 @@ void myVector(void)
 
 void main(void)
 {
-	TRISD = 0x00;
-	TRISB = 0x00;
-	TRISAbits.TRISA0 = 1;
-	ADCON0 = 0x41;
-	ADCON1 = 0x84;
-	INTCONbits.GIE = 1;
-	INTCONbits.PEIE = 1;	
-	PIE1bits.ADIE = 1;
-	PIR1bits.ADIF = 0;
-	flag = 0x01;
+	TRISD = 0x00;			// OUTPUT
+	TRISB = 0x00;			// OUTPUT
+	TRISAbits.TRISA0 = 1;		// INPUT	
+	ADCON0 = 0x41;			// CHANNEL 0, ADC ON, FOSC/8 CLOCK SPEED
+	ADCON1 = 0x84;			// RIGHT JUSTIFIED, RA0 ANALOG
+	INTCONbits.GIE = 1;		// ENABLE INTERRUPT GLOBALY
+	INTCONbits.PEIE = 1;		// ENABLE PERIPHERAL
+	PIE1bits.ADIE = 1;		// ENABLE ADC INTERRUPT
+	PIR1bits.ADIF = 0;	
+	flag = 0x01;			
 	while(1)
 	{
-		if(flag)
+		if(flag)		IF FLAG NON ZERO THEN START CONVERSION 
 		{
 			acquisitionTime();
 			ADCON0bits.GO = 1;			
@@ -51,11 +51,11 @@ void main(void)
 }
 void acquisitionTime(void)
 {
-	T0CON = 0x48;	
-	TMR0L = 236;
+	T0CON = 0x48;			// TIMER0 MODE 8, NO PRESCALER
+	TMR0L = 236;			
 	INTCONbits.TMR0IF = 0;
-	T0CONbits.TMR0ON = 1;
-	while(!INTCONbits.TMR0IF);
+	T0CONbits.TMR0ON = 1;		// START TIMER 0
+	while(!INTCONbits.TMR0IF);	// POLLING TIMER 0 FLAG
 	INTCONbits.TMR0IF = 0;
 	T0CONbits.TMR0ON = 0;
 }
